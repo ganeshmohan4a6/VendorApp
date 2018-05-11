ecb5ca1d4563f0e1cb4184ab3d2303f8bd7daaed
ganeshmohan4a6 <ganeshmohan4A6@gmail.com>
2018-05-11 20:05:31 +0530

    Update ILocationService.java

--- src/com/app/service/ILocationService.java
+++ src/com/app/service/ILocationService.java
@@ -7,9 +7,7 @@ import com.app.model.Location;
 public interface ILocationService {
 	public int saveLocation(Location loc);
 	public List<Location> getAllLocs();
-	
 	public void deleteLocById(int locId);
-	
 	public Location getLocById(int locId);
 	public void updateLocation(Location loc);
 }
